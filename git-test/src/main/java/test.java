dd61de168aa677456806f64b4696014062874da0
Linjiaqiang <3108136635@qq.com>
2019-07-15 11:13:13 +0800

    再次添加了一个输出语句，并添加一个注释

--- git-test/src/main/java/test.java
+++ git-test/src/main/java/test.java
@@ -2,6 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("测试");
         //添加一行字符
-        System.out.println("添加了一个输出语句");
+        System.out.println("添加了一个输出语句");//测试解决冲突
     }
 }
